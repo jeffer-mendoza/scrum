55b20505203c70841973b3baee28b46e544e689b
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-02-24 11:27:12 -0500

    Se agrega reporte de imagenes

--- src/ManagementBundle/Controller/ReportController.php
+++ src/ManagementBundle/Controller/ReportController.php
@@ -32,7 +32,7 @@ class ReportController extends Controller
     /**
      * Generate user story in pdf format
      *
-     * @Route("/story/{id}", name="report_story_pdf")
+     * @Route("/story/{id}/pdf", name="report_story_pdf")
      * @Method("GET")
      * @return Response
      */
@@ -42,18 +42,47 @@ class ReportController extends Controller
         $html = $this->renderView('report/pdf.html.twig', array(
             'story' => $story
         ));
-        $nameFile = "GDE".$story->getId().".pdf";
+        $nameFile = "GD" . $story->getId() . ".pdf";
 
         return new Response(
             $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
             200,
             array(
                 'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'attachment; filename="'.$nameFile.'"'
+                'Content-Disposition' => 'attachment; filename="' . $nameFile . '"'
             )
         );
 
         return $this->render('report/pdf.html.twig', array('story' => $story));
+
+    }
+
+
+    /**
+     * Generate user story in pdf format
+     *
+     * @Route("/story/{id}/image", name="report_story_image")
+     * @Method("GET")
+     * @return Response
+     */
+    public
+    function storyImageAction(Story $story)
+    {
+
+        $html = $this->renderView('report/image.html.twig', array(
+            'story' => $story
+        ));
+        $nameFile = "GD" . $story->getId() . ".jpg";
+
+        return new Response(
+            $this->get('knp_snappy.image')->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type' => 'image/jpg',
+                'Content-Disposition' => 'attachment; filename="' . $nameFile . '"'
+            )
+        );
+
 
     }
 
